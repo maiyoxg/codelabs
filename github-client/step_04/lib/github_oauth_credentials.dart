45525a84f1ae3d77558ccb5b047936d26570e5a4
Flutter desktop app <maiyoxg@gmail.com>
2023-02-08 11:32:32 +0200

    Update github_oauth_credentials.dart

--- github-client/step_04/lib/github_oauth_credentials.dart
+++ github-client/step_04/lib/github_oauth_credentials.dart
@@ -13,8 +13,8 @@
 // limitations under the License.
 
 // TODO(CodelabUser): Create an OAuth App
-const githubClientId = 'YOUR_GITHUB_CLIENT_ID_HERE';
-const githubClientSecret = 'YOUR_GITHUB_CLIENT_SECRET_HERE';
+const githubClientId = '95d1f6ff616fe2c6130a';
+const githubClientSecret = '95d1f6ff616fe2c6130a';
 
 // OAuth scopes for repository and user information
 const githubScopes = ['repo', 'read:org'];
